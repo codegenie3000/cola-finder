bfcc8af90cf0a34aae3f849db1d19de01442d125
Jonathan Peralez <jperalez@gmail.com>
2018-07-06 15:31:46 -0700

    Removed console.log

--- client/src/reducers/restaurantReducers.js
+++ client/src/reducers/restaurantReducers.js
@@ -4,7 +4,6 @@ import { FETCH_RESTAURANTS } from "../actions/types";
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_RESTAURANTS:
-            console.log(action.payload.data);
             return _.mapKeys(action.payload.data, 'name');
         default:
             return state;
