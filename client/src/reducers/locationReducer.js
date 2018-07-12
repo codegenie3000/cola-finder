a46b3834278ccf137117b7203a30414cd7cbc837
Jonathan Peralez <jperalez@gmail.com>
2018-07-12 10:57:15 -0700

    Changed initial state to null

--- client/src/reducers/locationReducer.js
+++ client/src/reducers/locationReducer.js
@@ -1,6 +1,6 @@
 import { GET_LOCATION } from "../actions/types";
 
-export default function (state = {latitude: 0, longitude: 0}, action) {
+export default function (state = null, action) {
 // export default function (state = null, action) {
     switch (action.type) {
         case GET_LOCATION:
