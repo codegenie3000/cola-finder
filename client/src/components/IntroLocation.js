79370534da82654634bc45cb7c86b496410cde9a
Jonathan Peralez <jperalez@gmail.com>
2018-07-20 00:31:24 -0700

    Getting the form value from IntroLocation now works

--- client/src/components/IntroLocation.js
+++ client/src/components/IntroLocation.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {setSimpleFilterCola} from '../actions';
 import { Field, reduxForm } from 'redux-form';
 import {setLocation} from '../actions';
+import {setLocationByZip} from '../actions';
 import {withRouter} from 'react-router-dom';
 import {bindActionCreators} from 'redux';
 import { Grid, Row, Col, Jumbotron, Button, FormGroup, FormControl, Form, ControlLabel } from 'react-bootstrap';
@@ -37,7 +38,7 @@ const zipCodeTextComponent = (
     );
 };
 
-const CustomForm = (props) => {
+/*const CustomForm = (props) => {
     const { handleSubmit, pristine, reset, submitting} = props;
     return (
         <Form
@@ -60,12 +61,45 @@ const CustomForm = (props) => {
             >Use Zip</Button>
         </Form>
     );
-};
+};*/
+
+class CustomForm extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+    }
+
+    render() {
+
+        return (
+            <Form
+                onSubmit={this.props.handleSubmit(this.props.myFunction.bind(this))}
+            >
+
+                <Field
+                    name="zipCode"
+                    component={zipCodeTextComponent}
+                    validate={[required]}
+                    type="text"
+                    label="Zip Code"
+                />
+
+                <Button
+                    type="submit"
+                    bsStyle="primary"
+                    bsSize="large"
+                    disabled={this.props.submitting}
+                >Use Zip</Button>
+            </Form>
+        );
+    }
+}
 
 class IntroLocation extends Component {
-    runOnSurveySubmit(e) {
-        e.preventDefault();
-        console.log('ran');
+    runOnSurveySubmit(values) {
+        // e.preventDefault();
+        console.log(values);
+        // this.props.setLocationByZip()
     }
 
     setLocationAndRoute() {
@@ -100,7 +134,7 @@ class IntroLocation extends Component {
                 </Row>
                 <Row className="show-grid text-center">
                     <Col xs={12}>
-                        <CustomForm handleSubmit={this.runOnSurveySubmit}/>
+                        <CustomForm handleSubmit={this.props.handleSubmit} myFunction={this.runOnSurveySubmit} />
                     </Col>
                 </Row>
             </Grid>
@@ -223,7 +257,7 @@ const ReduxFormControl = ({input, ...props}) => {
 
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({
-        setLocation: setLocation
+        setLocationByZip: setLocationByZip
     }, dispatch);
 }
 
